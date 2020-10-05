4c82944df662697de8c81e5853cba06c090af658
Siarhei Zaretsky <z__s@mail.ru>
2020-10-05 22:10:54 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = parseInt(n)
+    n = Math.abs(n)
+    let str = n.toString()
+    let digits = str.split('');
+
+    let res = []
+    for (digit of digits) {
+        res.unshift(digit)
+    }
+    return res.join('')
 }
